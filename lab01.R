9a53351c85447fbb3ba8587fb40b7ec23dd6d54f
unknown <thjcheng@gmail.com>
2026-01-29 16:07:24 -0500

    lab01

---
+++ lab01.R
@@ -0,0 +1,80 @@
+# Question 1 --------------------------------------------------------------
+rm(list=ls())
+library(tidyverse)
+library(ggplot2)
+d <- read.csv("data/lab01 dataset/ebchf3.csv") 
+# specify local filepath, in quotations
+
+# Question 2 --------------------------------------------------------------
+#View the first few rows of the data.
+head(d)
+
+
+# Question 3 -------------------------------------------------------------
+table(d$csmok)
+
+d %>% 
+  group_by(csmok) %>%
+  summarise(n=n()) %>%
+  ungroup()
+
+d %>% 
+  group_by(csmok) %>%
+  summarise(n=n(),
+            pct = n()/nrow(d)) %>%
+  ungroup()
+
+table(d$csmok)/nrow(d)
+
+# Question 4 --------------------------------------------------------------
+#Create a subset of the data with only smokers
+d.smok <- d %>% filter(csmok==1)
+
+# Question 5 --------------------------------------------------------------
+#Perform a two-sample t-test (unequal variances) evaluating whether the mean SBP differs by current smoking status
+d.nonsmok <- d %>% filter(csmok==0)
+t.test(d.smok$sbp, d.nonsmok$sbp)
+
+# Question 6 --------------------------------------------------------------
+#Fit a linear regression evaluating whether the mean SBP differs by smoking status. Write down the model that you are fitting. Interpret the estimated regression coefficients. According to the model, what is the average SBP among current smokers?
+fit.csmok <- lm(sbp ~ csmok, data = d)
+summary(fit.csmok)
+
+# Question 7 -------------------------------------------------------------
+
+
+
+# Question 8 --------------------------------------------------------------
+
+
+
+
+# Question 9 --------------------------------------------------------------
+
+
+
+
+
+
+# Question 10 -------------------------------------------------------------
+
+
+
+
+
+
+# Question 11 -------------------------------------------------------------
+
+
+
+
+
+# Question 12 -------------------------------------------------------------
+
+
+
+
+
+
+
+
